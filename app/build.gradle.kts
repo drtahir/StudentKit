ffe72cf3f7f48cdc60cbf0be61479ff4aef2d84c
drtahir <drtahirbunery@gmail.com>
2026-08-22 21:52:55 +0500

    fix: restore correct applicationId and package structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.studentkit.kxmpzq"
+    applicationId = "com.studentkit.buner"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
